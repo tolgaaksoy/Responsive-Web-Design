dc9bd67c51e7b9e93922badb333fd83a6d83bffe
Tolga Aksoy <tolgaaksoy@email.com>
2020-11-30 01:28:09 +0300

    security section completed

--- index.js
+++ index.js
@@ -8,14 +8,35 @@ $(document)
             dots: false,
             nav: true,
             responsive: {
-                0:{
-                    items:1
+                0: {
+                    items: 1
                 },
-                600:{
-                    items:2
+                600: {
+                    items: 2
                 },
-                1000:{
-                    items:3
+                1000: {
+                    items: 3
+                }
+            }
+        });
+        //security owl carousel
+        $('#security .owl-carousel').owlCarousel({
+            margin: 40,
+            dots: false,
+            loop: true,
+            nav: true,
+            navText: [
+                "<span><i class='fas fa-arrow-left'></i>Previous</span", "<span>Next<i class='fas fa-arrow-right'></i></span>"
+            ],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
                 }
             }
         });
