0583f71172afe555d01c42264c185b0ca603c39c
Tolga Aksoy <tolgaaksoy@email.com>
2020-11-30 00:41:04 +0300

    technology section made responsive...

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ $(document)
         $('#tech .owl-carousel').owlCarousel({
             margin: 40,
             dots: false,
-            nav: true
+            nav: true,
+            responsive: {
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:3
+                }
+            }
         });
-
     });
