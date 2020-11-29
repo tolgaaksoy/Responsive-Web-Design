cd2319abd8ace1b096cb71365c9b66a0420529bf
Tolga Aksoy <tolgaaksoy@email.com>
2020-11-30 00:33:07 +0300

    technology section completed

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 $(document)
     .ready(function () {
         //technology owl carousel
-        $('#tech .owl-carousel').owlCarousel();
+        $('#tech .owl-carousel').owlCarousel({
+            margin: 40,
+            dots: false,
+            nav: true
+        });
 
     });
